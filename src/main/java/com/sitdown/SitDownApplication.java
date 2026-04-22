f6dcb02bc48091cc1cc894eab3620280cf26c755
Mac <whaud1597@naver.com>
2026-04-22 22:44:30 +0900

    첫 커밋

---
+++ src/main/java/com/sitdown/SitDownApplication.java
@@ -0,0 +1,13 @@
+package com.sitdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitDownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitDownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sitdown/SitDownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitDownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
